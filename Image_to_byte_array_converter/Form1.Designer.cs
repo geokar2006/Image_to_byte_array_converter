e9a500d9c8bce30e8ffb2ae92650807f701de090
geokar2006 <54073773+geokar2006@users.noreply.github.com>
2020-06-25 18:47:00 +1000

    Add files via upload

---
+++ Image_to_byte_array_converter/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Image_to_byte_array_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть фото";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form_openfile);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(307, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Конвертировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form_convert);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 87);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(410, 507);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(86, 53);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form_Save);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(227, 53);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 28);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Сбросить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form_sbros);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(158, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 25);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Автор";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.avtor);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 606);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Конвертр картинок в byte array";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Image_to_byte_array_converter/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_to_byte_array_converter
+{
+    public partial class Form1 : Form
+    {
+        bool esl = false;
+        bool esl2 = false;
+        byte[] imageArray = null;
+        private OpenFileDialog ofd;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_openfile(object sender, MouseEventArgs e)
+        {
+            ofd = new OpenFileDialog();
+            ofd.Filter = "Картинки(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG" +
+            "|All files(*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                esl = true;
+                imageArray = File.ReadAllBytes(ofd.FileName);
+            }
+        }
+
+
+        private void Form_convert(object sender, MouseEventArgs e)
+        {
+            if (esl)
+            {
+                int lg = imageArray.Length;
+                richTextBox1.Text += "byte[] data = new byte[]{";
+                for (int i = 0; i < lg; ++i)
+                {
+                    if (i + 1 == lg)
+                    {
+                        if(imageArray[i] == 255)
+                        {
+                            richTextBox1.Text += "byte.MaxValue";
+                        }
+                        else
+                        {
+                            richTextBox1.Text += imageArray[i];
+                        }
+                        
+                    }
+                    else
+                    {
+                        if (imageArray[i] == 255)
+                        {
+                            richTextBox1.Text += "byte.MaxValue, ";
+                        }
+                        else
+                        {
+                            richTextBox1.Text += imageArray[i] + ", ";
+                        }
+                    }
+                }
+                richTextBox1.Text += "}";
+                MessageBox.Show("Файл конвертирован");
+                esl2 = true;
+            }
+            else
+            {
+                MessageBox.Show("Откройте Файл");
+            }
+        }
+
+        private void Form_Save(object sender, MouseEventArgs e)
+        {
+            if (esl2)
+            {
+                var sfd = new SaveFileDialog();
+                sfd.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+                if (sfd.ShowDialog() == DialogResult.Cancel)
+                    return;
+                string filename = sfd.FileName;
+                richTextBox1.SaveFile(filename, System.Windows.Forms.RichTextBoxStreamType.PlainText);
+                MessageBox.Show("Файл сохранён");
+            }
+            else
+            {
+                MessageBox.Show("Конвертируйте Файл");
+            }
+        }
+
+        private void Form_sbros(object sender, MouseEventArgs e)
+        {
+            esl = false;
+            esl2 = false;
+            richTextBox1.Text = "";
+            
+        }
+
+        private void avtor(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Автор:\r\ngeoakr2006\r\nYoutube:\r\ngeokar2006");
+        }
+    }
+}
